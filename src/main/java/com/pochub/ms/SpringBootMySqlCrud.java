8c20f5d4593b33a98dd9d9deab0c350e07be2e62
snmaddula <snmaddula@outlook.com>
2019-01-29 15:48:07 +0530

    v1 done'

---
+++ src/main/java/com/pochub/ms/SpringBootMySqlCrud.java
@@ -0,0 +1,12 @@
+package com.pochub.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqlCrud {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMySqlCrud.class, args);
+	}
+}
